1deef39d62fc75f4c996fad14b4111b76004b5ad
leonardo bravo <j.leonardo.bravo@gmail.com>
2021-09-06 18:15:08 -0500

    test: change jest config to run tests at each folder's root

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@jest/types";
 const config: Config.InitialOptions = {
   verbose: true,
   testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/src/jest-setup.ts"],
+  setupFilesAfterEnv: ["<rootDir>jest-setup.ts"],
   transform: {
     "^.+\\.[t|j]sx?$": "babel-jest",
   },
